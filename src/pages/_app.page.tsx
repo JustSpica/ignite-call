dc808156ab0b6cda2eda3f0a32c4edcd87c9b80f
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-05-05 13:34:11 -0300

    feat: created environment

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import { globalstyles } from '@styles/global'
+
+globalstyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ src/pages/index.page.tsx
@@ -0,0 +1,5 @@
+import { Heading } from '@ignite-ui/react'
+
+export default function Home() {
+  return <Heading as="h1">Hello world</Heading>
+}
